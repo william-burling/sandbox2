1e1bc0e0bf32ea5f39b8009c30bfd5490f36447a
William <william.burling@my.jcu.edu.au>
2018-08-16 15:32:14 +1000

    add list_files, give order of files inside repository

--- password_entry.py
+++ password_entry.py
@@ -1,14 +1,17 @@
 """William Burling"""
 
+
 def main():
     password = get_password()
     censor_password(password)
+
 
 def get_password():
     password = input("password: ")
     while len(password) < 5:
         password = input("invalid password ")
     return password
+
 
 def censor_password(password):
     for char in password:
